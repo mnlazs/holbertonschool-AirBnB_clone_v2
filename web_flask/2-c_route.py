5939231365978e8c7c2e0fcbb824ef71ef19225e
Your Name <you@example.com>
2023-03-27 17:53:44 +0000

    lol

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -8,13 +8,16 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def hello():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', methods=['GET'])
 def hbnb():
     return 'HBNB'
+
 
 @app.route('/c/<text>', methods=['GET'])
 def c_text(text):
@@ -22,4 +25,4 @@ def c_text(text):
     return 'C {}'.format(text)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True, use_reloader=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=True)
