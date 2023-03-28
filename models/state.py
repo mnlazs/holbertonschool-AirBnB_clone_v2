43bc782070717423af132f95048e7942b57c9aae
Your Name <you@example.com>
2023-03-28 18:03:58 +0000

    updaye

--- models/state.py
+++ models/state.py
@@ -19,14 +19,12 @@ class State(BaseModel, Base):
     else:
         name = ''
 
-        @property
-        def cities(self):
-            """
-                getter method, returns list of City objs from storage
-                linked to the current State
-            """
-            city_list = []
-            for city in models.storage.all("City").values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+
+@property
+def cities(self):
+        """Returns the list of City objects from State"""
+        cities = []
+        for city in models.storage.all(models.City).values():
+            if city.state_id == self.id:
+                cities.append(city)
+        return cities
