5d4b520f9a7559bec861d35ed8cca2ce285eb11e
Your Name <you@example.com>
2023-03-28 18:26:51 +0000

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,26 +2,30 @@
 """
     Sript that starts a Flask web application
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+
+    state_info = []
+    for state in sorted_states:
+        state_info.append("{}: <B>{}</B>".format(state.id, state.name))
+
+    return render_template('7-states_list.html', states=state_info)
+
+
 @app.teardown_appcontext
-def handle_teardown(self):
-    """
-        method to handle teardown
-    """
+def teardown_db(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def state_list():
-    """
-        method to render states
-    """
-    states = storage.all('State').values()
-    return render_template("7-states_list.html", states=states)
-
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
