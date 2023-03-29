307057a8de7a625e43a39c35193904908613ae51
Your Name <you@example.com>
2023-03-29 14:36:04 +0000

    updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-    script that starts a Flask web application
-"""
+""" Script that runs an app with Flask framework """
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+from models.city import City
+
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def appcontext_teardown(self):
-    """use storage for fetching data from the storage engine
-    """
+def teardown_session(exception):
+    """ Teardown """
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def state_info():
-    """Display a HTML page inside the tag BODY"""
+def display_html():
+    """ Function called with /states_list route """
+    states = storage.all(State)
     return render_template('8-cities_by_states.html',
-                           states=storage.all(State))
+                           Table="States",
+                           states=states)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
